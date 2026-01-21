6024bd9c8465b2c0644c68c0b86e45f38373dbf8
Areeba Muskan <aribamuskan40@gmail.com>
2026-01-22 00:17:40 +0500

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-600">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind v3 Working 🎉
+      </h1>
+    </div>
+  )
+}
